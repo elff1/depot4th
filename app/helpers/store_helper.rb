115420b59945daa3823ae0539e1223fa2b08b73a
elf <elff1@21cn.com>
2012-03-19 10:16:59 +0800

    add Check Cart on store index page

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,2 +1,9 @@
 module StoreHelper
+  def current_cart
+  	Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+  	cart = Cart.create
+  	session[:cart_id] = cart.id
+  	cart
+  end
 end
