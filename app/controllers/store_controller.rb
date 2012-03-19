d58e71918579cc057a7ae31aa719940f2f6014c1
elf <elff1@21cn.com>
2012-03-19 10:47:24 +0800

    Iteration F1: Moving the Cart to the store index page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,9 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+
+  	@cart = current_cart
+
   	if session[:counter].nil?
   		session[:counter] = 0
   	end
